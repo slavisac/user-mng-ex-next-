5d44d4e20b46c3ea5df3a4e7627379e9272693fd
slavisa <erm4ones>
2022-07-29 11:30:06 +0200

    use formik

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,113 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Copyright from '../components/copyright';
 import PrimaryAppBar from '../components/primaryAppBar';
-import { blue } from '@mui/material/colors';
 import Image from 'next/image';
+import * as Yup from 'yup';
 
 import headerLogo from '../public/header-logo.png';
-import backgroundImg from '../public/background.png';
+import { useRouter } from 'next/router';
+import { useFormik } from 'formik';
 
 const theme = createTheme();
 
-const styles = {
-  gridContainer: {
-      backgroundImage: `url(${backgroundImg})`,
-      minHeight: '80vh'
-  }
-};
+
+const Login = () => {
+
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email(
+          'Must be a valid email')
+        .max(255)
+        .required(
+          'Email is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required(
+          'Password is required')
+    }),
+    onSubmit: () => {
+      router.push('/table');
+    }
+  });
+
+  return (
+    <Box
+      sx={{
+        mx: 'auto',
+        mt: 15,
+        justifyContent: 'center',
+        boxShadow: 4,
+        display: 'flex',
+        padding: 5,
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Image src={headerLogo} width="135px" height="28.5px" alt="Logo" />
+      <Box component="form" onSubmit={formik.handleSubmit} sx={{ mt: 1 }}>
+        <TextField
+          error={Boolean(formik.touched.email && formik.errors.email)}
+          fullWidth
+          helperText={formik.touched.email && formik.errors.email}
+          label="Email Address"
+          margin="normal"
+          name="email"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          type="email"
+          value={formik.values.email}
+          variant="outlined"
+        />
+        <TextField
+          error={Boolean(formik.touched.password && formik.errors.password)}
+          fullWidth
+          helperText={formik.touched.password && formik.errors.password}
+          label="Password"
+          margin="normal"
+          name="password"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          type="password"
+          value={formik.values.password}
+          variant="outlined"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          disabled={formik.isSubmitting}
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link href="/table" variant="body2">
+              Forgot password?
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+
+}
 
 export default function SignIn() {
   const handleSubmit = (event) => {
@@ -44,67 +131,7 @@ export default function SignIn() {
       <PrimaryAppBar link="/about" />
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Grid container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"      
-          style={styles.gridContainer}>
-          <Box
-            sx={{
-              mx: 'auto',
-              justifyContent: 'center',
-              boxShadow: 4,
-              display: 'flex',
-              padding: 5,
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Image src={headerLogo} width="135px" height="28.5px" alt="Logo" />
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="/table" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
+        <Login />
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
