ffe4f93c595cf94efe6711d5d96731775691633b
slavisa <erm4ones>
2022-08-12 11:00:54 +0200

    fixed empty layout content

--- layout/EmptyLayout.js
+++ layout/EmptyLayout.js
@@ -1,14 +1,26 @@
 import { ThemeProvider } from "@emotion/react";
-import { Box, CssBaseline } from "@mui/material";
+import { Box, Button, CssBaseline, Typography } from "@mui/material";
+import Link from "next/link";
 import lightTheme from "../styles/theme/lightTheme";
 
-export default function EmptyLayout(){
+export default function EmptyLayout() {
 
     return (
         <ThemeProvider theme={lightTheme}>
-            <Box sx={{ display: "flex" }}>
-                <CssBaseline />
-                Page not found                
+            <Box className='content-center'>
+                <Box sx={{ p: 5, display: 'flex', flexDirection: 'column', mt: '10%', alignItems: 'center', textAlign: 'center' }}>
+                    <CssBaseline />
+                    <Typography variant='h1'>404</Typography>
+                    <Typography variant='h5' sx={{ mb: 1, fontSize: '1.5rem !important' }}>
+                        Page Not Found ⚠️
+                    </Typography>
+
+                    <Link passHref href='/'>
+                        <Button component='a' variant='contained' sx={{ px: 5.5 }}>
+                            Back to Home
+                        </Button>
+                    </Link>
+                </Box>
             </Box>
         </ThemeProvider>
     );
